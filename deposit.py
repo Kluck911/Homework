d14904cf6f24ea982efa10e0ca5784cee4ea6a8e
kluck911 <kluck1812@gmail.com>
2021-05-12 14:49:42 +0300

    EX 12.7.3

--- deposit.py
+++ deposit.py
@@ -1,15 +1,17 @@
+# создаем словарь с процентами депозитов
 per_cent = {'ТКБ': 5.6,
             'СКБ': 5.9,
             'ВТБ': 4.28,
             'СБЕР': 4.0}
+# вводим сумму депозита
 money = int(input("Please enter the deposit amount: "))
 deposit = []
 
 for value in per_cent.values():
-    deposit.append(round(float(money/100 * value)))
+    deposit.append(round(float(money/100 * value))) #создаем список с выплатами
 
 print("The deposits payments = ", deposit)
-deposit.sort()
-print("The maximum deposit payments = ", deposit[-1])
+deposit.sort()# сортировка списка по возрастанию значений
+print("The maximum deposit payments = ", deposit[-1]) # выводим последний элемент списка
 
 
